0b0998fda5e42033ca6b69aa040d24f37bcebae0
snehachaudhary05 <@chaudharysneha693.com>
2024-11-07 23:09:52 +0530

    complete signup page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { SignUp } from "./pages/SignUp"
 import { Profile } from "./pages/Profile"
 import { About } from "./pages/About";
 import { Header } from "./components/Header"
+
 export default function App() {
   return <BrowserRouter>
   <Header />
