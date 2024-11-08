7af416eb9eae0b649ce3d10df912f588c0b7acbc
snehachaudhary05 <@chaudharysneha693.com>
2024-11-08 23:43:38 +0530

    complete signup page  ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 export const SignUp = () => {
+  const [formData,setFormData]=useState({})
+  const handleChange=(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]:e.target.value,
+    });
+  };
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    const res=await fetch('/api/auth/signup',
+      {
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify(formData),
+      });
+      const data=await res.json();
+  }
+  console.log(formData);
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibol my-7'>
         Sign Up
       </h1>
-      <form className='flex flex-col gap-4' >
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4' >
         <input type="text" 
-        placeholder='username ' className='border p-3 rounded-lg' id='username'/>
+        placeholder='username ' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
         <input type="text" 
-        placeholder='email ' className='border p-3 rounded-lg' id='email'/>
+        placeholder='email ' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
                 <input type="text" 
-        placeholder='password ' className='border p-3 rounded-lg' id='password'/>
+        placeholder='password ' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
         <button  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80' >Sign up</button>
       </form>
       <div className='flex gap-2 mt-5'>
